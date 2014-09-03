1219d7dcce689b2600dae42ae6159019d2a6a605
Nicolas Byl <nicolas.byl@netpioneer.de>
2014-09-03 07:19:48 +0200

    - moved config location setting to application

--- service/src/main/java/com/github/nbyl/xfdcontrol/service/Application.java
+++ service/src/main/java/com/github/nbyl/xfdcontrol/service/Application.java
@@ -16,17 +16,39 @@
 
 package com.github.nbyl.xfdcontrol.service;
 
+import com.google.common.base.StandardSystemProperty;
+import com.google.common.base.Strings;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import java.io.File;
+
 
 @ComponentScan("com.github.nbyl.xfdcontrol")
 @EnableAutoConfiguration
 @EnableWebMvc
 public class Application {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
+    public static final String SPRING_CONFIG_LOCATION = "spring.config.location";
+
     public static void main(String[] args) {
+        installConfigLocation();
+
         SpringApplication.run(Application.class, args);
+    }
+
+    private static void installConfigLocation() {
+        if (Strings.isNullOrEmpty(System.getProperty(SPRING_CONFIG_LOCATION))) {
+            LOGGER.debug("Loading configuration from user home directory {}.", StandardSystemProperty.USER_HOME.value());
+            File configFile = new File(StandardSystemProperty.USER_HOME.value(), ".xfdcontrol.properties");
+            System.setProperty(SPRING_CONFIG_LOCATION, configFile.getAbsolutePath());
+        } else {
+            LOGGER.debug("Configuration location already set.");
+        }
     }
 }
